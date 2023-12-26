5f821c3ba4712346a29eec2aa0e371e7cb095ae5
Megidd <megiddgit@gmail.com>
2023-12-26 04:04:18 -0800

    Material props

--- RhinoCommon/BESO/BESO/BESOTopologyOptimization.cs
+++ RhinoCommon/BESO/BESO/BESOTopologyOptimization.cs
@@ -30,6 +30,39 @@ namespace BESO
             {
                 return Result.Failure;
             }
+
+            // Units of measurement:
+            // https://engineering.stackexchange.com/q/54454/15178
+            double MassDensity = 0; // (N*s2/mm4)
+            double YoungModulus = 0; // MPa (N/mm2)
+            double PoissonRatio = 0;
+
+            uint materialProps = Helper.GetUint32FromUser("Enter material/metal type. Gold=1, Silver=2, Steel=3", 3, 1, 3);
+            switch (materialProps)
+            {
+                case 1:
+                    // Gold
+                    MassDensity = 19.3e-9; // (N*s2/mm4) // 19.3 g/cm3
+                    YoungModulus = 79000; // MPa (N/mm2) // 79 GPa
+                    PoissonRatio = 0.4;
+                    break;
+                case 2:
+                    // Silver
+                    MassDensity = 10.49e-9; // (N*s2/mm4) // 10.49 g/cm3
+                    YoungModulus = 83000; // MPa (N/mm2) // 83 GPa
+                    PoissonRatio = 0.37;
+                    break;
+                case 3:
+                    // Steel
+                    MassDensity = 7.85e-9; // (N*s2/mm4) // 7.85 g/cm3
+                    YoungModulus = 210000; // MPa (N/mm2) // 210 GPa
+                    PoissonRatio = 0.3;
+                    break;
+                default:
+                    RhinoApp.WriteLine("It's out of range");
+                    return Result.Failure;
+            }
+
             return Result.Success;
         }
     }
